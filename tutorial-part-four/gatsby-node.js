8b04e68285f4b84671ab05759957a2da0bf961fc
Courtney Adams <cadams8238@gmail.com>
2018-10-13 11:35:39 -0700

    added basic pages for blog posts

--- tutorial-part-four/gatsby-node.js
+++ tutorial-part-four/gatsby-node.js
@@ -1,5 +1,5 @@
 "use strict";
-
+const path = require("path");
 const { createFilePath } = require(`gatsby-source-filesystem`);
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -13,4 +13,36 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 			value: slug
 		});
 	}
+};
+
+exports.createPages = ({ graphql, actions }) => {
+	const { createPage } = actions;
+	return new Promise((resolve, reject) => {
+		graphql(`
+			{
+				allMarkdownRemark {
+					edges {
+						node {
+							fields {
+								slug
+							}
+						}
+					}
+				}
+			}
+		`).then(result => {
+			result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+				createPage({
+					path: node.fields.slug,
+					component: path.resolve(`./src/templates/blog-post.js`),
+					context: {
+						// Data passed to context is available
+						// in page queries as GraphQL variables.
+						slug: node.fields.slug
+					}
+				});
+			});
+			resolve();
+		});
+	});
 };
